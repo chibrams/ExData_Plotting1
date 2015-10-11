a3327edae26495979d155e52cbfd4715f66585dc
chibrams <chibrams@gmail.com>
2015-10-12 01:55:45 +0200

    hi

---
+++ Plot3.R
@@ -0,0 +1,18 @@
+Plot3 <- function() {
+  
+  subMetering1 <- as.numeric(subSetData$Sub_metering_1)
+  subMetering2 <- as.numeric(subSetData$Sub_metering_2)
+  subMetering3 <- as.numeric(subSetData$Sub_metering_3)
+  DateTime<- subSetData$DateTime
+  
+  png("plot3.png", width=480, height=480)
+  
+  plot(DateTime, subMetering1, type="l", ylab="Energy Submetering", xlab="")
+  lines(DateTime, subMetering2, type="l", col="red")
+  lines(DateTime, subMetering3, type="l", col="blue")
+  
+  legend("topright", c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty=1, lwd=2.5, col=c("black", "red", "blue"))
+  dev.off()
+}
+
+Plot3()
