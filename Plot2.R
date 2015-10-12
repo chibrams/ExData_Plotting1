32bf38a16988d729d629a6d7c1f9a7952368a8e5
chibrams <chibrams@gmail.com>
2015-10-12 08:01:01 +0200

    Delete Plot2.R

--- Plot2.R
+++
@@ -1,6 +0,0 @@
-plot2 <- function() {
-  plot(subSetData$DateTime,subSetData$Global_active_power, type="l", xlab="", ylab="Global Active Power (kilowatts)")
-  dev.copy(png, file="plot2.png", width=480, height=480)
-  dev.off()
-}
-plot2()
