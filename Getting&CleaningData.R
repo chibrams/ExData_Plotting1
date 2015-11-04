7c663b13423a213de68735497f396aef12262b35
chibrams <chibrams@gmail.com>
2015-11-03 22:56:56 -0500

    Merge branch 'master' of https://github.com/chibrams/ExData_Plotting1
    Conflicts:
    	Getting&CleaningData.R
    	Plot1.R
    	Plot2.R
    	Plot3.R
    	Plot4.R

--- Getting&CleaningData.R
+++ Getting&CleaningData.R
@@ -4,12 +4,15 @@ if(!file.exists("./data")){dir.create("./data")}
 fileUrl <- "http://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
 download.file(fileUrl,destfile="./data/HPCDataset.zip",method="auto")
 dataFile<-unzip(zipfile="./data/HPCDataset.zip",exdir="./data")
+
 # Subsetting data to the 1st two days of february (to speed up the processing).
 HouseHoldData<- read.table(dataFile, header=T, sep=";")
 HouseHoldData$Date <- as.Date(HouseHoldData$Date, format="%d/%m/%Y")
 subSetData <- HouseHoldData[(HouseHoldData$Date=="2007-02-01") | (HouseHoldData$Date=="2007-02-02"),]
+
 # Convert the Date and Time variables to Date/Time classes.
 subSetData <- transform(subSetData , DateTime=as.POSIXct(paste(Date, Time)), "%d/%m/%Y %H:%M:%S")
+
 # Convert other columns to the correct types
 subSetData $Global_active_power <- as.numeric(as.character(subSetData $Global_active_power))
 subSetData $Global_reactive_power <- as.numeric(as.character(subSetData $Global_reactive_power))
@@ -18,9 +21,8 @@ subSetData $Global_intensity <- as.numeric(subSetData $Global_intensity)
 subSetData $Sub_metering_1 <- as.numeric(as.character(subSetData $Sub_metering_1))
 subSetData $Sub_metering_2 <- as.numeric(as.character(subSetData $Sub_metering_2))
 subSetData $Sub_metering_3 <- as.numeric(as.character(subSetData $Sub_metering_3))
+
 # Cleaning workspace from data source files.
-<<<<<<< HEAD
+
 unlink("data", recursive=TRUE)
-=======
-unlink("data", recursive=TRUE)
->>>>>>> a59f93ffe3cb1d6017e1386a580444be11fc53af
+
